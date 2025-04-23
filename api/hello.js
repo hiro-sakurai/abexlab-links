717378f30ffe7253ddc4d9cbb5f1c0f65a4083df
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-04-23 18:51:46 +0500

    1

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,16 @@
+const cors = require('cors');
+
 module.exports = (req, res) => {
+  // CORS sozlamalari
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Bu * o'rniga faqat kerakli domenni kiritishingiz mumkin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   res.status(200).json({ message: "success!" });
 };
+
